b86340f78d3fa5fe2c4fc60315752d4511a42852
NothingVX <61274720+NothingVX@users.noreply.github.com>
2020-08-07 05:35:01 +0700

    First

---
+++ main/java/com/example/linearandrelative/LinearAndActivity2.java
@@ -0,0 +1,14 @@
+package com.example.linearandrelative;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinearAndActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_and2);
+    }
+}
