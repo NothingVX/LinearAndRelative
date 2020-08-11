5e8a586fde32fe220c1919e987d8e792afbbf4f1
NothingVX <61274720+NothingVX@users.noreply.github.com>
2020-08-11 08:59:50 +0700

    main
    TableAndFrame

---
+++ TableAndFrame/java/com/example/tableframelayout/TableFrameLayout2.java
@@ -0,0 +1,14 @@
+package com.example.tableframelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TableFrameLayout2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_frame_layout2);
+    }
+}
